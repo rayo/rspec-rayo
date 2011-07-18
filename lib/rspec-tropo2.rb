e3024d86485b2261c21ea656b2f03f3a1d41edfb
Ben Langfeld <ben@langfeld.me>
2011-07-18 11:35:38 +0100

    Separate driver files by Tropo version

--- lib/rspec-tropo2.rb
+++ lib/rspec-tropo2.rb
@@ -1,6 +1,7 @@
 %w{
-  rspec-tropo2/tropo2-driver
-  rspec-tropo2/tropo2-call
-  rspec-tropo2/tropo1-driver
-  rspec-tropo2/matchers
+  rspec-tropo2/tropo1/driver
+
+  rspec-tropo2/tropo2/driver
+  rspec-tropo2/tropo2/call
+  rspec-tropo2/tropo2/matchers
 }.each { |lib| require lib }
