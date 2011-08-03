90d1df2a9dee3092a1b9a905affcb749b74c8747
Ben Langfeld <ben@langfeld.me>
2011-08-03 12:33:48 +0100

    Allow matching the call ID and mixer ID on joined/unjoined events

--- lib/rspec-rayo/rayo/matchers/join.rb
+++ lib/rspec-rayo/rayo/matchers/join.rb
@@ -1,6 +1,17 @@
 RSpec::Matchers.define :be_a_valid_joined_event do
+  chain :with_other_call_id do |other_call_id|
+    @other_call_id = other_call_id
+  end
+
+  chain :with_mixer_id do |mixer_id|
+    @mixer_id = mixer_id
+  end
+
   match_for_should do |event|
-    basic_validation event, Punchblock::Protocol::Rayo::Event::Joined
+    basic_validation event, Punchblock::Protocol::Rayo::Event::Joined do
+      @error = "The other call ID was not correct. Expected '#{@other_call_id}', got '#{event.other_call_id}'" if @other_call_id && event.other_call_id != @other_call_id
+      @error = "The mixer ID was not correct. Expected '#{@mixer_id}', got '#{event.mixer_id}'" if @mixer_id && event.mixer_id != @mixer_id
+    end
   end
 
   failure_message_for_should do |actual|
@@ -8,13 +19,27 @@ RSpec::Matchers.define :be_a_valid_joined_event do
   end
 
   description do
-    "be a valid joined event"
+    "be a valid joined event".tap do |d|
+      d << " with other call ID '#{@other_call_id}'" if @other_call_id
+      d << " with mixer ID '#{@mixer_id}'" if @mixer_id
+    end
   end
 end
 
 RSpec::Matchers.define :be_a_valid_unjoined_event do
+  chain :with_other_call_id do |other_call_id|
+    @other_call_id = other_call_id
+  end
+
+  chain :with_mixer_id do |mixer_id|
+    @mixer_id = mixer_id
+  end
+
   match_for_should do |event|
-    basic_validation event, Punchblock::Protocol::Rayo::Event::Unjoined
+    basic_validation event, Punchblock::Protocol::Rayo::Event::Unjoined do
+      @error = "The other call ID was not correct. Expected '#{@other_call_id}', got '#{event.other_call_id}'" if @other_call_id && event.other_call_id != @other_call_id
+      @error = "The mixer ID was not correct. Expected '#{@mixer_id}', got '#{event.mixer_id}'" if @mixer_id && event.mixer_id != @mixer_id
+    end
   end
 
   failure_message_for_should do |actual|
@@ -22,7 +47,10 @@ RSpec::Matchers.define :be_a_valid_unjoined_event do
   end
 
   description do
-    "be a valid unjoined event"
+    "be a valid unjoined event".tap do |d|
+      d << " with other call ID '#{@other_call_id}'" if @other_call_id
+      d << " with mixer ID '#{@mixer_id}'" if @mixer_id
+    end
   end
 end
 
--- spec/rspec-rayo/rayo/matchers/join_spec.rb
+++ spec/rspec-rayo/rayo/matchers/join_spec.rb
@@ -3,23 +3,31 @@ require 'spec_helper'
 describe "Rayo call control matchers" do
   describe "a joined event" do
     subject do
-      Punchblock::Protocol::Rayo::Event::Joined.new.tap do |event|
+      Punchblock::Protocol::Rayo::Event::Joined.new(:other_call_id => 'foo', :mixer_id => 'foo1').tap do |event|
         event.call_id = '5d6fe904-103d-4551-bd47-cf212c37b8c7'
         event.command_id = '6d5bf745-8fa9-4e78-be18-6e6a48393f13'
       end
     end
 
     it { should be_a_valid_joined_event }
+    it { should be_a_valid_joined_event.with_other_call_id('foo') }
+    it { should_not be_a_valid_joined_event.with_other_call_id('bar') }
+    it { should be_a_valid_joined_event.with_mixer_id('foo1') }
+    it { should_not be_a_valid_joined_event.with_mixer_id('bar1') }
   end
 
   describe "an unjoined event" do
     subject do
-      Punchblock::Protocol::Rayo::Event::Unjoined.new.tap do |event|
+      Punchblock::Protocol::Rayo::Event::Unjoined.new(:other_call_id => 'foo', :mixer_id => 'foo1').tap do |event|
         event.call_id = '5d6fe904-103d-4551-bd47-cf212c37b8c7'
         event.command_id = '6d5bf745-8fa9-4e78-be18-6e6a48393f13'
       end
     end
 
     it { should be_a_valid_unjoined_event }
+    it { should be_a_valid_unjoined_event.with_other_call_id('foo') }
+    it { should_not be_a_valid_unjoined_event.with_other_call_id('bar') }
+    it { should be_a_valid_unjoined_event.with_mixer_id('foo1') }
+    it { should_not be_a_valid_unjoined_event.with_mixer_id('bar1') }
   end
 end
