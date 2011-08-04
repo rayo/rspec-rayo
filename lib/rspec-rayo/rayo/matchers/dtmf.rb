6fc05098bd51861533071c7b821093c345c457eb
Ben Langfeld <ben@langfeld.me>
2011-08-04 17:58:04 +0100

    Allow asserting DTMF signal

--- lib/rspec-rayo/rayo/matchers/dtmf.rb
+++ lib/rspec-rayo/rayo/matchers/dtmf.rb
@@ -1,6 +1,12 @@
 RSpec::Matchers.define :be_a_valid_dtmf_event do
+  chain :with_signal do |signal|
+    @signal = signal
+  end
+
   match_for_should do |event|
-    basic_validation event, Punchblock::Protocol::Rayo::Event::DTMF
+    basic_validation event, Punchblock::Protocol::Rayo::Event::DTMF do
+      @error = "The signal was not correct. Expected '#{@signal}', got '#{event.signal}'" if @signal && event.signal != @signal
+    end
   end
 
   failure_message_for_should do |actual|
@@ -8,6 +14,8 @@ RSpec::Matchers.define :be_a_valid_dtmf_event do
   end
 
   description do
-    "be a valid DTMF event"
+    "be a valid DTMF event".tap do |d|
+      d << " with signal '#{@signal}'" if @signal
+    end
   end
 end
--- spec/rspec-rayo/rayo/matchers/dtmf_spec.rb
+++ spec/rspec-rayo/rayo/matchers/dtmf_spec.rb
@@ -5,9 +5,12 @@ describe "Rayo DTMF matchers" do
     subject do
       Punchblock::Protocol::Rayo::Event::DTMF.new.tap do |dtmf_event|
         dtmf_event.call_id = '5d6fe904-103d-4551-bd47-cf212c37b8c7'
+        dtmf_event.signal = 3
       end
     end
 
     it { should be_a_valid_dtmf_event }
+    it { should be_a_valid_dtmf_event.with_signal('3') }
+    it { should_not be_a_valid_dtmf_event.with_signal('5') }
   end
 end
