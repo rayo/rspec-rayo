786fc8414e33fc0a5a088d3f9691db57547f165c
Ben Langfeld <ben@langfeld.me>
2011-08-05 11:10:02 +0100

    Add a matcher for stopped recording

--- lib/rspec-rayo/rayo/matchers/recording.rb
+++ lib/rspec-rayo/rayo/matchers/recording.rb
@@ -6,10 +6,27 @@ RSpec::Matchers.define :be_a_valid_complete_recording_event do
   end
 
   failure_message_for_should do |actual|
-    "The say event was not valid: #{@error}"
+    "The recording event was not valid: #{@error}"
   end
 
   description do
     "be a valid complete recording event"
+  end
+end
+
+RSpec::Matchers.define :be_a_valid_stopped_recording_event do
+  match_for_should do |event|
+    basic_validation event, Punchblock::Protocol::Rayo::Event::Complete, true do
+      match_type event.reason, Punchblock::Protocol::Rayo::Event::Complete::Stop
+      match_type event.recording, Punchblock::Protocol::Rayo::Command::Record::Recording
+    end
+  end
+
+  failure_message_for_should do |actual|
+    "The recording event was not valid: #{@error}"
+  end
+
+  description do
+    "be a valid stopped recording event"
   end
 end
